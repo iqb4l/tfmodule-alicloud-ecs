08e2e0e0d29d77f303ccbe7736190e6583739021
Iqb4l <iqbalwahid51@gmail.com>
2025-10-24 21:35:13 +0700

    Initial commit: ecs module

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,3 @@ output "private_ip" {
 output "public_ip" {
   value = var.allocate_public_ip ? alicloud_eip_address.eip[0].ip_address : null
 }
-
-output "security_group_id" {
-  value = length(var.security_group_rules) > 0 ? alicloud_security_group.ecs_sg[0].id : null
-}
